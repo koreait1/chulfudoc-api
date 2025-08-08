778711eeb93ff86f9936789841bd0dde6cf60170
juyonghyeon <jyh928578@gmail.com>
2025-08-08 18:18:17 +0900

    08. 08 공통소스 작업

--- src/main/java/org/backend/chulfudoc/global/configs/CorsConfig.java
+++ src/main/java/org/backend/chulfudoc/global/configs/CorsConfig.java
@@ -18,7 +18,9 @@ public class CorsConfig {
         config.addAllowedOrigin("*");
         // config.setAllowCredentials(true); 브라우저가 Ajax, fetch 요청 시 쿠키·세션·Authorization 헤더 같은 인증 정보를 포함할 수 있도록
 
+        // CORS(Cross-Origin Resource Sharing) 정책 때문에 막히는 Ajax 요청을 허용하기 위해 사용
         source.registerCorsConfiguration("/api/v1/**", config);
+        source.registerCorsConfiguration("/file/**", config);
 
         return new CorsFilter(source);
     }
